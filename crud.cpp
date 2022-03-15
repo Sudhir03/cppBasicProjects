6fce64644544749ed9e7825baf8e6b7cc7f6ea16
Sudhir Sharma <88575370+Sudhir03@users.noreply.github.com>
2022-03-15 12:12:23 +0530

    Update crud.cpp

--- crud.cpp
+++ crud.cpp
@@ -1,53 +1,60 @@
 using namespace std;
+
 #include <iostream>
+#include <string>
 #include <fstream>
 
 class Student
 {
 public:
     int roll;
+    string name;
     Student *next;
 
     Student()
     {
     }
 
-    Student(int r)
+    Student(int r, string n)
     {
         roll = r;
+        name = n;
         next = NULL;
     }
 };
 
+void insertAtTail(Student *&head, int r, string n)
+{
+
+    Student *p = new Student(r, n);
+
+    if (head == NULL)
+    {
+        head = p;
+        return;
+    }
+
+    Student *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = p;
+}
+
 void display(Student *head)
 {
     Student *temp = head;
-    while (temp != NULL)
-    {
-        // cout << temp->roll << " " << temp->name << endl;
-        cout << temp->roll << endl;
-        temp = temp->next;
-    }
-}
-
-void insertAtTail(Student *&head, int r)
-{
-
-    Student *n = new Student(r);
     if (head == NULL)
     {
-        head = n;
+        cout << "No records to show" << endl;
+        return;
     }
-    else
-    {
-        Student *temp = head;
-        while (temp->next == NULL)
-        {
-            cout << "in inserattail" << endl;
 
-            temp = temp->next;
-        }
-        temp->next = n;
+    while (temp != NULL)
+    {
+        cout << temp->roll << "\t" << temp->name << endl;
+        temp = temp->next;
     }
 }
 
@@ -85,39 +92,84 @@ void insertAtTail(Student *&head, int r)
 
 void search(Student *head, int key)
 {
+    bool flag;
     Student *temp = head;
     while (temp != NULL)
     {
         if (temp->roll == key)
         {
             cout << "Record found with roll number " << key << endl;
-            cout << temp->roll << endl;
+            cout << temp->roll << "\t" << temp->name << endl;
+            flag = true;
             break;
         }
         temp = temp->next;
     }
-    cout << "Record not found wit roll number " << key << endl;
+    if (!flag)
+    {
+        cout << "Record not found wit roll number " << key << endl;
+    }
 }
 
 void updateRecord(Student *&head, int key)
 {
+    bool flag;
+    string name;
+    int choice;
     Student *temp = head;
     while (temp != NULL)
     {
         if (temp->roll == key)
         {
-            cout << "Enter new data to update records" << endl;
-            cin >> temp->roll;
+            cout << "Record found with roll number " << key << endl;
+            cout << temp->roll << "\t" << temp->name << endl;
+            cout << "Press 1 to update roll number:" << endl;
+            cout << "Press 2  to update name:" << endl;
+            cout << "Press 3 to update both:" << endl;
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                cout << "Enter new roll number:" << endl;
+
+                cin >> temp->roll;
+                break;
+            case 2:
+                cout << "Enter new name:" << endl;
+                cin.ignore();
+                getline(cin, name);
+                temp->name = name;
+                break;
+            case 3:
+                cout << "Enter new roll number:" << endl;
+                cin >> temp->roll;
+                cout << "Enter new name:" << endl;
+                cin.ignore();
+                getline(cin, name);
+                temp->name = name;
+                break;
+            default:
+                cout << "Invalid Choice!" << endl;
+                break;
+            }
+
             cout << "Record updated successfully" << endl;
+            cout << "Updated record: " << temp->roll << "\t" << temp->name << endl;
+            flag = true;
             break;
         }
         temp = temp->next;
     }
-    cout << "Record not found wit roll number " << key << endl;
+    if (!flag)
+    {
+        cout << "Record not found wit roll number " << key << endl;
+    }
 }
 
 void deleteRecord(Student *&head, int key)
 {
+    bool flag;
     Student *temp = head;
     while (temp != NULL)
     {
@@ -125,11 +177,17 @@ void deleteRecord(Student *&head, int key)
         {
             temp = temp->next->next;
             cout << "Record deleted successfully" << endl;
+            flag = true;
+
             break;
         }
         temp = temp->next;
     }
-    cout << "Record not found wit roll number " << key << endl;
+    head = temp;
+    if (!flag)
+    {
+        cout << "Record not found wit roll number " << key << endl;
+    }
 }
 
 int menu()
@@ -147,79 +205,73 @@ int menu()
 }
 
 int main()
+
 {
     Student *head;
-    insertAtTail(head, 1);
-    insertAtTail(head, 2);
-    insertAtTail(head, 3);
-    insertAtTail(head, 4);
-    insertAtTail(head, 5);
-    display(head);
+
+    int key, roll;
+    char y;
+    string name;
+
+    while (1)
+    {
+        switch (menu())
+        {
+        case 1:
+            cout << "Enter details of student" << endl;
+            do
+            {
+                cout << "Enter roll number: ";
+                cin >> roll;
+                cout << "Enter name: " << endl;
+                cin.ignore();
+
+                getline(cin, name);
+
+                insertAtTail(head, roll, name);
+                cout << "Enter y to add more records... ";
+                cin >> y;
+            } while (y == 'y' || y == 'Y');
+            break;
+        case 2:
+            display(head);
+            break;
+        case 3:
+            do
+            {
+                cout << "Enter roll number to search for a record: ";
+                cin >> key;
+                search(head, key);
+                cout << "Enter y to search for more records... ";
+                cin >> y;
+            } while (y == 'y' || y == 'Y');
+            break;
+        case 4:
+            do
+            {
+                cout << "Enter roll number to update record" << endl;
+                cin >> key;
+                updateRecord(head, key);
+                cout << "Enter y to update more records... ";
+                cin >> y;
+            } while (y == 'y' || y == 'Y');
+            break;
+        case 5:
+            do
+            {
+                cout << "Enter roll number to delete record" << endl;
+                cin >> key;
+                deleteRecord(head, key);
+                cout << "Enter y to delete more records... ";
+                cin >> y;
+            } while (y == 'y' || y == 'Y');
+            break;
+        case 6:
+            exit(6);
+            break;
+        default:
+            cout << "Invalid Choice!" << endl;
+        }
+    }
     return 0;
 }
-
-// int main()
-// {
-//     int key, roll;
-//     char y;
-
-//     Student *head;
-//     while (1)
-//     {
-//         // system("cls");
-
-//         switch (menu())
-//         {
-//         case 1:
-//             cout << "Enter details of student" << endl;
-//             do
-//             {
-//                 cout << "Enter roll number of student: ";
-//                 cin >> roll;
-//                 insertAtTail(head, roll);
-//                 cout << "Enter y to add more records... ";
-//                 cin >> y;
-//             } while (y == 'y' || y == 'Y');
-//             break;
-//         case 2:
-//             display(head);
-//             break;
-//         case 3:
-//             do
-//             {
-//                 cout << "Enter roll number to search for a record: ";
-//                 cin >> key;
-//                 search(head, key);
-//                 cout << "Enter y to search for more records... ";
-//                 cin >> y;
-//             } while (y == 'y' || y == 'Y');
-//             break;
-//         case 4:
-//             do
-//             {
-//                 cout << "Enter roll number to update record" << endl;
-//                 cin >> key;
-//                 updateRecord(head, key);
-//                 cout << "Enter y to update more records... ";
-//                 cin >> y;
-//             } while (y == 'y' || y == 'Y');
-//             break;
-//         case 5:
-//             do
-//             {
-//                 cout << "Enter roll number to delete record" << endl;
-//                 cin >> key;
-//                 deleteRecord(head, key);
-//                 cout << "Enter y to delete more records... ";
-//                 cin >> y;
-//             } while (y == 'y' || y == 'Y');
-//             break;
-//         case 6:
-//             exit(6);
-//             break;
-//         default:
-//             cout << "Invalid Choice!" << endl;
-//         }
-//     }
-//     return 0;
-// }
